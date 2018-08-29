b81da18b817eb277b53aeb1d1b4df41999fc9007
Dave Nicholson <fatnic388@gmail.com>
2018-08-29 11:16:01 +0100

    improved line of sight code

--- scripts/line_of_sight.gd
+++ scripts/line_of_sight.gd
@@ -1,16 +1,22 @@
-extends Node2D
+extends Node
 
 var target = null
 var seen = false
+var hit_point = null
+
 
 func _process(delta):
 
 	seen = false
+	hit_point = null
+
 	
 	if target:
-		var space_state = get_world_2d().direct_space_state
-		var result = space_state.intersect_ray(global_position, target.global_position, [get_parent()], get_parent().collision_mask)
+		
+		var space_state = get_parent().get_world_2d().direct_space_state
+		var result = space_state.intersect_ray(get_parent().global_position, target.global_position, [get_parent()], get_parent().collision_mask)
 		
 		if result:
+			hit_point = result.position
 			if result.collider == target:
 				seen = true
